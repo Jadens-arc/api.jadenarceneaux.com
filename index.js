8b90a5375fcaf576741aba321423163121609578
Jaden Arceneaux <contact@jadenarceneaux.com>
2025-02-20 21:56:16 -0800

    raised rate limit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 100 requests per windowMs
+  max: 30, // limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again later.'
 });
 
